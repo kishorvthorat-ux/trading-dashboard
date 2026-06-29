ca9a7c02440cf834b3392ade5d9cd9dcd718b6f6
kishorvthorat-ux <kishorvthorat@gmail.com>
2026-06-29 12:27:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add current directory to path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 st.write(os.listdir())
 
 import streamlit as st
